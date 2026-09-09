1ceacc5ec34ff40112eb70db56074dab4035d720
Erickson Salangsang <sonsonyyy20@gmail.com>
2026-09-10 01:39:42 +0800

    feat(observability): add Sentry error monitoring

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
+use Sentry\Laravel\Integration;
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -24,5 +26,5 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        Integration::handles($exceptions);
     })->create();
